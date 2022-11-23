e3b5f3964de33e745a0c9bd56d98dfc741523014
Ricardo Ortiz <40183604+rjop95@users.noreply.github.com>
2022-11-22 23:46:08 -0600

    second commit

--- Merge Sort/src/mergeSort/MergeSort.java
+++ Merge Sort/src/mergeSort/MergeSort.java
@@ -72,7 +72,7 @@ public class MergeSort {
  
     void display(int arr[])                 //display the array
     {  
-    	System.out.println("Hola ");
+    	System.out.println("Hola 2");
         for (int i=0; i<arr.length; ++i) 
         {
             System.out.print(arr[i]+" ");
